229b88031918a6866e996162b27ad59464333f65
enorma <enorma29@gmail.com>
2020-10-15 22:11:27 -0500

    rbtrees 2

--- rbt/rbt.c
+++ rbt/rbt.c
@@ -21,6 +21,14 @@ Node p  = {'\0', black, NULL, NULL, NULL};
 Tree t  = {NULL, &p};
 Tree *T = &t;
 
+void printNode(char* msg, Node *x) {
+    if(x==T->nil) {
+        printf("%s, NULL\n", msg);
+        return;
+    }
+    printf("%s, key: %d, color: %s\n", msg, x->key, x->color==red?"red":"black");
+}
+
 void leftRotate(Tree *T, Node *x) {
 
     Node *y  = x->right;
@@ -80,16 +88,18 @@ void insertFixup(Tree *T, Node *z){
                 y->color       = black;
                 z->p->p->color = red;
                 z              = z->p->p;
-            }else if(z == z->p->right) {
-                z = z->p;
+            }else {
 
-                leftRotate(T,z);
+                if(z == z->p->right) {
+                    z = z->p;
+                    leftRotate(T,z);
+                }
+
+                z->p->color    = black;
+                z->p->p->color = red;
+
+                rightRotate(T, z->p->p);
             }
-
-            z->p->color    = black;
-            z->p->p->color = red;
-
-            rightRotate(T, z->p->p);
         }else {
 
             Node *y = z->p->p->left;
@@ -99,19 +109,22 @@ void insertFixup(Tree *T, Node *z){
                 y->color       = black;
                 z->p->p->color = red;
                 z              = z->p->p;
-            }else if(z == z->p->left) {
-                z = z->p;
-                leftRotate(T,z);
+            }else {
+
+                if(z == z->p->left) {
+                    z = z->p;
+                    leftRotate(T,z);
+                }
+
+                z->p->color    = black;
+                z->p->p->color = red;
+
+                leftRotate(T, z->p->p);
             }
-
-            z->p->color    = black;
-            z->p->p->color = red;
-
-            leftRotate(T, z->p->p);
         }
-
-        T->root->color = black;
     }
+
+    T->root->color = black;
 }
 
 void insert(Tree *T, Node *z) {
@@ -153,24 +166,56 @@ void insert(Tree *T, Node *z) {
     insertFixup(T, z);
 }
 
+Node* search(Node *N, int k) {
+
+    if(N == T->nil) {
+        return T->nil;
+    }else if(k == N->key) {
+        return N;
+    }else if(k < N->key) {
+        return search(N->left, k);
+    }else if(N->key < k) {
+        return search(N->right, k);
+    }
+
+    return T->nil;
+}
+
 int main() {
 
     printf("holamundo!!\n");
-    printf("Color de T: %d\n", T->nil->key);
 
-    Node s = {10, black, T->nil, T->nil, T->nil};
-    Node q = {52, black, T->nil, T->nil, T->nil};
-    Node r = {70, black, T->nil, T->nil, T->nil};
-    //Node u = {70, black, T->nil, T->nil, T->nil};
+    Node s = {10, red, T->nil, T->nil, T->nil};
+    Node q = {5,  red, T->nil, T->nil, T->nil};
+    Node r = {3,  red, T->nil, T->nil, T->nil};
+    Node u = {1,  red, T->nil, T->nil, T->nil};
+    Node v = {0,  red, T->nil, T->nil, T->nil};
+    Node w = {2,  red, T->nil, T->nil, T->nil};
 
     insert(T, &s);
     insert(T, &q);
     insert(T, &r);
+    insert(T, &u);
+    insert(T, &v);
+    insert(T, &w);
 
-    printf("Key de root:  %d\n", T->root->key);
-    printf("Key de left:  %d\n", T->root->left->key);
-    printf("Key de right: %d\n", T->root->right->key);
-    printf("holamundo");
+    printNode("root", T->root);
+    printNode("root  -> left",  T->root->left);
+    printNode("root  -> right", T->root->right);
+    printNode("left  -> left",  T->root->left->left);
+    printNode("left  -> right", T->root->left->right);
+    printNode("right -> left",  T->root->right->left);
+    printNode("right -> right", T->root->right->right);
+    printNode("l->l  -> left",  T->root->left->left->left);
+    printNode("l->l  -> right", T->root->left->left->right);
+    printNode("l->r  -> left",  T->root->left->right->left);
+    printNode("l->r  -> right", T->root->left->right->right);
+
+    Node *a = search(T->root, 666);
+
+    printNode("searched", a);
+
+    printf("holamundo\n");
 
     return 0;
 }
